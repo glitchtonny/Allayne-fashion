a95a07882ac8148cba5b9543ac9ef4a74220379a
g;itchtonny <sirmwangitosh@gmail.com>
2024-08-27 10:33:12 +0300

    commit changes

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,3 +1,33 @@
+// import React, { useState, useEffect } from 'react';
+// import './Banner.css'; 
+
+// const Banner = ({ images }) => {
+//   const [currentIndex, setCurrentIndex] = useState(0);
+
+//   useEffect(() => {
+//     const interval = setInterval(() => {
+//       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+//     }, 5000); 
+
+//     return () => clearInterval(interval);
+//   }, [images.length]);
+
+//   return (
+//     <div className="banner">
+//       <img src={images[currentIndex]} alt="Banner" />
+//     </div>
+//   );
+// };
+
+// export default Banner;
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////
+
+
 import React, { useState, useEffect } from 'react';
 import './Banner.css'; 
 
@@ -7,14 +37,22 @@ const Banner = ({ images }) => {
   useEffect(() => {
     const interval = setInterval(() => {
       setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
-    }, 5000); 
+    }, 6000); 
 
     return () => clearInterval(interval);
   }, [images.length]);
 
+  const image1 = images[currentIndex];
+  const image2 = images[(currentIndex + 1) % images.length]; // The next image for the second column
+
   return (
     <div className="banner">
-      <img src={images[currentIndex]} alt="Banner" />
+      <div className="banner-images">
+      <div className="banner-image" style={{ backgroundImage: `url(${image1})` }} />
+
+        <div className="banner-image" style={{ backgroundImage: `url(${image2})` }} />
+
+      </div>
     </div>
   );
 };
--- src/Components/ShopAll.jsx
+++ src/Components/ShopAll.jsx
@@ -150,9 +150,15 @@ const ShopAll = () => {
     <div className='cont'>
        <Banner images={[
         'https://assets.vogue.com/photos/5ee7e4567760032c3ba72260/master/w_2560%2Cc_limit/00_story.jpg',
+        'https://img.freepik.com/free-photo/graceful-french-female-model-enjoying-portrait-sensual-caucasian-girl-red-beret-black-skirt_197531-20339.jpg',
         'https://img.freepik.com/free-photo/cute-woman-bright-hat-purple-blouse-is-leaning-stand-with-dresses-posing-with-package-isolated-background_197531-17610.jpg',
         'https://opinionatedmale.com/wp-content/uploads/2013/04/NYFW-black-women-Fashion-OpinionatedMaleblog-1140x803.jpg',
         'https://img.freepik.com/free-photo/fashion-photo-graceful-brunette-woman-spring-outfit-posing-holding-white-bag_273443-4124.jpg',
+        'https://cdn.dribbble.com/users/9355434/screenshots/17357989/media/03ed746ec611b7a7eda3ecbc64a9f528.jpg?resize=400x0',
+        'https://cdn.create.vista.com/downloads/bd6cf32a-34ed-4568-aa85-53b0e84aa3ea_1024.jpeg',
+        'https://www.stylerave.com/wp-content/uploads/2022/02/0FC53C4C-1C59-44AC-BC45-EE899BEE906E.jpeg',
+        'https://img.freepik.com/free-photo/good-humoured-young-girl-white-gumshoes-posing-with-ice-cream-serious-lady-beret-sitting-floor-eating-dessert_197531-20451.jpg?size=626&ext=jpg&ga=GA1.1.2008272138.1720915200&semt=ais_user',
+        'https://images.indianexpress.com/2019/10/short-girl-dressing-tips.jpg?w=414'
         
         ]} />
        <SearchBar onSearch={handleSearch} />
