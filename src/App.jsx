28020125439333e5ab926f5b2d627df7c9e89b7d
brnomosa <omosabria99@gmail.com>
2024-08-27 14:38:45 +0300

    add changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import ProductForm from './components/product'  
-import UserDashboard from './components/UserDashboard'
-import ProductDetail from './components/ProductDetail'
-import Cartpage from './components/cartpage'
+import ProductForm from './pages/product'  
+import UserDashboard from './pages/UserDashboard'
+import Cartpage from './pages/cartpage'
 
 function App() {
   
@@ -12,9 +11,8 @@ function App() {
   return (
    <Router>
    <Routes>
-    <Route path="/product" element={<ProductForm />} />
+    <Route path="/productform" element={<ProductForm />} />
     <Route path="/UserDashboard" element={<UserDashboard />} />
-    <Route path="/ProductDetail" element={<ProductDetail />} />
     <Route path="/cartpage" element={<Cartpage />} />
     
     
