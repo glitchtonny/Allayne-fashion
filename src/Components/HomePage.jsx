3b3c4a16c524a2f59a9673df94a95981a2553cbd
g;itchtonny <sirmwangitosh@gmail.com>
2024-08-30 17:55:48 +0300

    commit changes

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -9,13 +9,15 @@ const HomePage = () => {
 
       <div className="hero-section">
         <img
-          src="https://img.freepik.com/premium-vector/online-shop-neon-signs-vector-design-template-neon-style_77399-1178.jpg" 
+          src="https://t4.ftcdn.net/jpg/06/10/57/75/360_F_610577574_QMoYRcv8AUUYl3SJPvXUtBCkUYbxmw8d.jpg" 
           alt="Fashion Banner"
           className="hero-image"
         />
         <div className="hero-text">
-          <h1>Unique Fashion</h1>
-          <p>For Women</p>
+        <h1>Allayne Fashion</h1>
+          <h2>Merging Style with Elegance</h2>
+          <p>Unique Women Fashion</p>
+          <p></p>
         </div>
       </div>
 
