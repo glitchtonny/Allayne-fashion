1d77a08f00d5da0c754844e688394ae99d4249a6
g;itchtonny <sirmwangitosh@gmail.com>
2024-08-30 17:02:54 +0300

    add carousel

---
+++ src/Components/Courasel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Courasel.css';
+
+const Carousel = () => {
+  return (
+    <div className="carousel">
+      <img src="https://i5.walmartimages.com/seo/7-3-Inch-Android-13-Smart-Phone-16GB-SIM-Standby-7800mAh-4G-Network-Mobile-Phone_56569106-ca36-4fe0-b2b6-750a51285803.94170a00c00d10188f0def058ee76890.jpeg" alt="Slide 1" />
+      <img src="https://img.freepik.com/premium-vector/online-shop-neon-signs-vector-design-template-neon-style_77399-1178.jpg" alt="Slide 2" />
+      <img src="https://img.freepik.com/premium-vector/online-shop-neon-signs-vector-design-template-neon-style_77399-1178.jpg" alt="Slide 3" />
+    </div>
+  );
+};
+
+export default Carousel;
