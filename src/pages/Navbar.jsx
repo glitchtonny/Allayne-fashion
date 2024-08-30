9ccdc6ae7e94edb387db5012192478e392f3df2c
g;itchtonny <sirmwangitosh@gmail.com>
2024-08-30 17:06:21 +0300

    adding user icon

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -3,16 +3,27 @@ import { Link } from 'react-router-dom';
 import '../styles/Navbar.css';
 
 function Navbar({ isLoggedIn }) {
+  // Retrieve the user data from localStorage
+  const user = isLoggedIn ? JSON.parse(localStorage.getItem('user')) : null;
+
   return (
     <nav>
-      <Link to="/" className="nav-logo"><img src='styles/Logo.png'></img></Link> {/* Your logo or brand name */}
+      <Link to="/" className="nav-logo">
+        <img src="src/images/allayne0-removebg-preview.png" alt="Logo" />
+      </Link>
       <ul className="nav-links">
         <li><Link to="/">Home</Link></li>
         <li><Link to="/shop">Shop All</Link></li>
         <li><Link to="/about">About</Link></li>
         <li>
           {isLoggedIn ? (
-            <Link to="/account">Account</Link>
+            <Link to={user?.role === 'admin' ? '/AdminDashboard' : '/UserDashboard'}>
+              <img 
+                src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png" 
+                alt="User Dashboard" 
+                className="user-icon" 
+              />
+            </Link>
           ) : (
             <Link to="/login">Login</Link>
           )}
